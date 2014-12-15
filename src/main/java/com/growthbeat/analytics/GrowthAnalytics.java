923339ed4ace4785584bc009fec44a61657d66b9
Shigeru Ogawa <ogawa@sirok.co.jp>
2014-12-15 18:04:36 +0900

    Add ClientEvent function.

--- src/main/java/com/growthbeat/analytics/GrowthAnalytics.java
+++ src/main/java/com/growthbeat/analytics/GrowthAnalytics.java
@@ -1,8 +1,11 @@
 package com.growthbeat.analytics;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import com.growthbeat.Context;
+import com.growthbeat.analytics.model.ClientEvent;
 import com.growthbeat.analytics.model.Event;
 import com.growthbeat.analytics.model.Metric;
 import com.growthbeat.analytics.model.Tag;
@@ -90,6 +93,22 @@ public class GrowthAnalytics {
 
 	public void deleteMetric(String metricId) {
 		Metric.delete(metricId, context);
+	}
+
+	public ClientEvent findClientEventById(String id) {
+		return ClientEvent.findById(id, context);
+	}
+
+	public List<ClientEvent> findClientEventsByClientId(String clientId, Date from, Date to) {
+		return ClientEvent.findByClientId(clientId, from, to, context);
+	}
+
+	public List<ClientEvent> findClientEventsByEventId(String eventId, Date from, Date to) {
+		return ClientEvent.findByEventId(eventId, from, to, context);
+	}
+
+	public ClientEvent createClientEvent(String clientId, String eventId, Map<String, String> properties) {
+		return ClientEvent.create(clientId, eventId, properties, context);
 	}
 
 }
