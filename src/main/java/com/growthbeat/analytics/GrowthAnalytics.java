0e6b9cabaf90ea00a4b8b236a1a8abc869b4ba94
Shigeru Ogawa <ogawa@sirok.co.jp>
2014-12-15 16:59:57 +0900

    Create GrowthAnalytics class.

---
+++ src/main/java/com/growthbeat/analytics/GrowthAnalytics.java
@@ -0,0 +1,16 @@
+package com.growthbeat.analytics;
+
+public class GrowthAnalytics {
+
+	private static final String BASE_URL = "https://api.analytics.growthbeat.com";
+
+	public GrowthAnalytics() {
+		super();
+	}
+
+	public GrowthAnalytics(String credentialId) {
+		this();
+
+	}
+
+}
