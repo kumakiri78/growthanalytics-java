90dd970280c3dadb087a53c60853b7644d5aefba
Shigeru Ogawa <ogawa@sirok.co.jp>
2014-12-15 17:05:18 +0900

    Update GrowthAnalytics initialize method.

--- src/main/java/com/growthbeat/analytics/GrowthAnalytics.java
+++ src/main/java/com/growthbeat/analytics/GrowthAnalytics.java
@@ -1,16 +1,20 @@
 package com.growthbeat.analytics;
 
+import com.growthbeat.Context;
+
 public class GrowthAnalytics {
 
 	private static final String BASE_URL = "https://api.analytics.growthbeat.com";
+	private Context context = new Context();
 
 	public GrowthAnalytics() {
 		super();
+		context.getGrowthbeatHttpClient().setBaseUrl(BASE_URL);
 	}
 
 	public GrowthAnalytics(String credentialId) {
 		this();
-
+		context.setCredentialId(credentialId);
 	}
 
 }
