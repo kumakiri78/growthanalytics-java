fa29ddfdf4231cc17012250de445fe74f4cde7e9
Shigeru Ogawa <ogawa@sirok.co.jp>
2014-12-15 17:57:40 +0900

    Add Event function .

--- src/main/java/com/growthbeat/analytics/GrowthAnalytics.java
+++ src/main/java/com/growthbeat/analytics/GrowthAnalytics.java
@@ -1,10 +1,13 @@
 package com.growthbeat.analytics;
 
+import java.util.List;
+
 import com.growthbeat.Context;
+import com.growthbeat.analytics.model.Event;
 
 public class GrowthAnalytics {
 
-	private static final String BASE_URL = "https://api.analytics.growthbeat.com";
+	private final String BASE_URL = "https://api.analytics.growthbeat.com";
 	private Context context = new Context();
 
 	public GrowthAnalytics() {
@@ -23,6 +26,26 @@ public class GrowthAnalytics {
 
 	public void setContext(Context context) {
 		this.context = context;
+	}
+
+	public Event findById(String id) {
+		return Event.findById(id, context);
+	}
+
+	public List<Event> findByApplicationId(String applicationId) {
+		return Event.findByApplicationId(applicationId, context);
+	}
+
+	public Event create(String parentEventId, String applicationId, String name, String description) {
+		return Event.create(parentEventId, applicationId, name, description, context);
+	}
+
+	public Event update(String eventId, String description) {
+		return Event.update(eventId, description, context);
+	}
+
+	public void delete(String eventId) {
+		Event.delete(eventId, context);
 	}
 
 }
